8a4deccfbb503a2b03fd8f0037b517908fde48c8
Michael <miichaaeell.andrew@gmail.com>
2025-10-17 13:57:31 -0300

    created js file for cashier flow page

---
+++ enterprise/static/js/flow_cashier.js
@@ -0,0 +1,13 @@
+function flowCashier() {
+  return {
+
+    donwloadRelatorio(id){
+
+      console.log(id)
+        notificationModal.show({
+              title: 'Download do Fluxo de Caixa',
+              message: 'O download do fluxo de caixa está em construção.',
+              })
+    },
+  };
+}
