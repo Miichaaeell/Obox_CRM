533580b234024e1c90cf70cffe314da59e4c0586
Michael <miichaaeell.andrew@gmail.com>
2025-11-02 12:44:00 -0300

    created service models

--- enterprise/models.py
+++ enterprise/models.py
@@ -35,6 +35,12 @@ class Enterprise(TimeStampedModel, models.Model):
     def __str__(self):
         return self.name
 
+class Services(TimeStampedModel, models.Model):
+    service = models.CharField(max_length=50, unique=True, verbose_name='Serviço')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Preço')
+
+    def __str__(self):
+        return f'{self.service} - {self.price}'
 
 class StatusBill(TimeStampedModel, models.Model):
     status = models.CharField(max_length=100)
