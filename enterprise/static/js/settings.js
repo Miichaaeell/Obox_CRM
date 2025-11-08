83adc7e0032ee8b9987c2ca6aaa4b3753052b9d8
Michael <miichaaeell.andrew@gmail.com>
2025-11-08 14:00:17 -0300

    Logic created to update plan.

--- enterprise/static/js/settings.js
+++ enterprise/static/js/settings.js
@@ -2,6 +2,8 @@ function SettingsHandler(){
     return{
         menu:'plans',
         formData:{},
+        mode:'create',
+        url:'',
 
         async SubmitForm(){
             const csrf = document.querySelector('[name=csrfmiddlewaretoken]').value
@@ -9,17 +11,18 @@ function SettingsHandler(){
                 'Content-Type': 'application/json',
                 'X-CSRFToken': csrf,
             };
-            const url = document.getElementById('create_plan').dataset.url
-            const response = await fetch(url, {
-                method: 'POST',
+            const fetch_url = this.mode == 'create' ? document.getElementById('create_plan').dataset.url : this.url
+            const method = this.mode == 'create' ? 'POST' : 'PUT'
+            const response = await fetch(fetch_url, {
+                method: method,
                 headers: headers,
                 body: JSON.stringify(this.formData)
-            })
-            if(!response.ok){
-                console.log('Erro ao realizar o fetch para a API')
-            }else{
-                location.reload()
-            }
+                })
+                if(!response.ok){
+                    console.log('Erro ao realizar o fetch para a API')
+                }else{
+                    location.reload()
+                }
         },
 
         async DeletePlan(url){
@@ -37,6 +40,15 @@ function SettingsHandler(){
             }else{
                 location.reload()
             }
+        },
+
+        async EditPlan(url){
+            const response = await fetch(url)
+            const data = await response.json()
+            this.formData = data
+            this.mode = 'update'
+            this.url = url
+            this.menu = 'create-plan'
         }
     }
 }
