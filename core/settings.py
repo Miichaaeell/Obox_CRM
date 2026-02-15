573eb2acf323da8a837d12b8408864be501cf107
Michael Andrew <miichaaeell.andrew@gmail.com>
2026-02-15 18:37:58 -0300

    Retirado variável base domain do settings

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", False)
 
-base_domain = config("BASE_DOMAIN")
 ALLOWED_HOSTS = [
     config("ALLOWED_HOSTS", default="", cast=Csv()),
 ]
